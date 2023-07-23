de1817baaecabcdf7156f471086cea22f2162665
theakgit <kakash2198@gmail.com>
2023-07-23 20:29:12 +0530

    last commit

--- src/GitClass.java
+++ src/GitClass.java
@@ -4,6 +4,8 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("GIT repository from JAAVA");
   System.out.println("hi there");
+  System.out.println("Akash");
+  System.out.println("kya haal hai");
 	}
 
 }
